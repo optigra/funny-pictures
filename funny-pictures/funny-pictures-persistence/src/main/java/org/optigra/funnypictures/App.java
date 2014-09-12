0187807e44302aa40e2d47fc1a89b10a8d4a3d26
Ivan Ursul <i.ursul@optigra.org>
2014-09-12 19:24:35 +0300

    Initial commit

---
+++ funny-pictures/funny-pictures-persistence/src/main/java/org/optigra/funnypictures/App.java
@@ -0,0 +1,13 @@
+package org.optigra.funnypictures;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
