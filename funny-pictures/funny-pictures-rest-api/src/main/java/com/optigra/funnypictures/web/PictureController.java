1fb3e008f0b25a3b8b1cacb84d29e4903240dc5b
Ivan Ursul <i.ursul@optigra.org>
2014-09-15 15:12:15 +0300

    Added initial context for spring

--- funny-pictures/funny-pictures-rest-api/src/main/java/com/optigra/funnypictures/web/PictureController.java
+++ funny-pictures/funny-pictures-rest-api/src/main/java/com/optigra/funnypictures/web/PictureController.java
@@ -1,8 +1,16 @@
 package com.optigra.funnypictures.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PictureController {
 
+	@RequestMapping(value = "/")
+	@ResponseBody
+	public String method1() {
+		return "Hello";
+	}
+	
 }
