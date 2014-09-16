349fec6073c978f42d62037793dca811b9a6c1c4
Ivan Ursul <ivan.ursul@optigra-soft.com>
2014-09-16 16:24:00 +0300

    Added message converters

--- funny-pictures/funny-pictures-rest-api/src/main/java/com/optigra/funnypictures/web/PictureController.java
+++ funny-pictures/funny-pictures-rest-api/src/main/java/com/optigra/funnypictures/web/PictureController.java
@@ -3,12 +3,20 @@ package com.optigra.funnypictures.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.optigra.funnypictures.facade.resources.Picture;
+
 @RestController
 public class PictureController {
 
 	@RequestMapping(value = "/")
-	public String method1() {
-		return "Hello";
+	public Picture method1() {
+		
+		String name = "name";
+		
+		Picture picture = new Picture();
+		picture.setName(name);
+		
+		return picture;
 	}
 	
 }
