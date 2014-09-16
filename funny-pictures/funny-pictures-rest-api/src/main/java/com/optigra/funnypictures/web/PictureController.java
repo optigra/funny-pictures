2a51fa137d2b0cfb77bde88f3b529ebaf35f9938
Ivan Ursul <ivan.ursul@optigra-soft.com>
2014-09-16 15:02:38 +0300

    added property placeholder configurer

--- funny-pictures/funny-pictures-rest-api/src/main/java/com/optigra/funnypictures/web/PictureController.java
+++ funny-pictures/funny-pictures-rest-api/src/main/java/com/optigra/funnypictures/web/PictureController.java
@@ -1,14 +1,12 @@
 package com.optigra.funnypictures.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PictureController {
 
 	@RequestMapping(value = "/")
-	@ResponseBody
 	public String method1() {
 		return "Hello";
 	}
