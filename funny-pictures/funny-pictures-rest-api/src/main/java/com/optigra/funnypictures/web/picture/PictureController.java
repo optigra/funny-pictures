cd6a258e96cd93123ff5d373e8e3d28e4e009af1
Ivan Ursul <ivan.ursul@optigra-soft.com>
2014-09-17 18:04:09 +0300

    Removed logger

--- funny-pictures/funny-pictures-rest-api/src/main/java/com/optigra/funnypictures/web/picture/PictureController.java
+++ funny-pictures/funny-pictures-rest-api/src/main/java/com/optigra/funnypictures/web/picture/PictureController.java
@@ -2,8 +2,6 @@ package com.optigra.funnypictures.web.picture;
 
 import javax.annotation.Resource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
