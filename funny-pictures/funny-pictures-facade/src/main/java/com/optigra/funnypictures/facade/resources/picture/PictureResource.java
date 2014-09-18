98b23f84855459ad82fdcbbec5a80b81dc2043d0
Oleg Yatskiv <o.yatskiv@optigra.org>
2014-09-18 12:44:52 +0300

    Added PictureConverster

--- funny-pictures/funny-pictures-facade/src/main/java/com/optigra/funnypictures/facade/resources/picture/PictureResource.java
+++ funny-pictures/funny-pictures-facade/src/main/java/com/optigra/funnypictures/facade/resources/picture/PictureResource.java
@@ -45,4 +45,43 @@ public class PictureResource extends ApiResource {
 				+ "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((url == null) ? 0 : url.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PictureResource other = (PictureResource) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (url == null) {
+			if (other.url != null)
+				return false;
+		} else if (!url.equals(other.url))
+			return false;
+		return true;
+	}
+	
+	
+
 }
