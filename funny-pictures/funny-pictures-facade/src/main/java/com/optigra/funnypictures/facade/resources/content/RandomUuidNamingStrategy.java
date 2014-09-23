8623b4ae010ebd8947d54f83674ffc3d60753d28
Rostyslav Dyyak <rostyslav.dyyak@optigra-soft.com>
2014-09-23 12:48:45 +0300

    Fixed bug with namingStrategy bean creating. Changed main page layout. Improved header and footer style. Changed funny-pictures.css.

--- funny-pictures/funny-pictures-facade/src/main/java/com/optigra/funnypictures/facade/resources/content/RandomUuidNamingStrategy.java
+++ funny-pictures/funny-pictures-facade/src/main/java/com/optigra/funnypictures/facade/resources/content/RandomUuidNamingStrategy.java
@@ -2,6 +2,10 @@ package com.optigra.funnypictures.facade.resources.content;
 
 import java.util.UUID;
 
+import org.springframework.stereotype.Component;
+
+
+@Component("namingStrategy")
 public class RandomUuidNamingStrategy implements ContentResourceNamingStrategy {
 
 	@Override
