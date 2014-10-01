0160a75fa16f209e3745193da8c815ded5461c3c
Oleg Yatskiv <o.yatskiv@optigra.org>
2014-10-01 17:08:36 +0300

    Added first simple scenario for get pictures.

--- funny-pictures/funny-pictures-integration/src/test/scala/com/optigra/funnypictures/integration/PictureScenario.scala
+++ funny-pictures/funny-pictures-integration/src/test/scala/com/optigra/funnypictures/integration/PictureScenario.scala
@@ -7,8 +7,8 @@ import scala.concurrent.duration._
 class PictureScenario extends Simulation {
 
   val httpConf = http
-    .baseURL("http://computer-database.herokuapp.com") // Here is the root for all relative URLs
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
+    .baseURL("http://localhost:8080/funny-pictures-rest-api/api") // Here is the root for all relative URLs
+    .acceptHeader("text/html,application/xhtml+xml,application/xml,application/json;q=0.9,*/*;q=0.8") // Here are the common headers
     .doNotTrackHeader("1")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
@@ -16,41 +16,10 @@ class PictureScenario extends Simulation {
 
   val headers_10 = Map("Content-Type" -> """application/x-www-form-urlencoded""") // Note the headers specific to a given request
 
-  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(http("request_1")
-      .get("/"))
-    .pause(7) // Note that Gatling has recorder real time pauses
-    .exec(http("request_2")
-      .get("/computers?f=macbook"))
-    .pause(2)
-    .exec(http("request_3")
-      .get("/computers/6"))
-    .pause(3)
-    .exec(http("request_4")
-      .get("/"))
-    .pause(2)
-    .exec(http("request_5")
-      .get("/computers?p=1"))
-    .pause(670 milliseconds)
-    .exec(http("request_6")
-      .get("/computers?p=2"))
-    .pause(629 milliseconds)
-    .exec(http("request_7")
-      .get("/computers?p=3"))
-    .pause(734 milliseconds)
-    .exec(http("request_8")
-      .get("/computers?p=4"))
-    .pause(5)
-    .exec(http("request_9")
-      .get("/computers/new"))
-    .pause(1)
-    .exec(http("request_10") // Here's an example of a POST request
-      .post("/computers")
-      .headers(headers_10)
-      .formParam("""name""", """Beautiful Computer""") // Note the triple double quotes: used in Scala for protecting a whole chain of characters (no need for backslash)
-      .formParam("""introduced""", """2012-05-30""")
-      .formParam("""discontinued""", """""")
-      .formParam("""company""", """37"""))
+  val scn = scenario("Test Pictures") // A scenario is a chain of requests and pauses
+    .exec(http("Request Pictures")
+      .get("/pictures"))
+    
 
   setUp(scn.inject(atOnceUsers(1)).protocols(httpConf))
 }
