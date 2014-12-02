391c25b2f251835db7c637ad7c9836d50c0b614a
Myroslav Golub <myroslav.golub@optigra-soft.com>
2014-12-02 18:23:59 +0200

    Added a stub for picture annotating service.

---
+++ funny-pictures/funny-pictures-service/src/main/java/com/optigra/funnypictures/service/marking/PictureAnnotatingService.java
@@ -0,0 +1,13 @@
+package com.optigra.funnypictures.service.marking;
+
+import com.optigra.funnypictures.content.model.Content;
+
+public class PictureAnnotatingService implements PictureMarkingService {
+
+	@Override
+	public Content generateMarkedPicture(Content source) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ funny-pictures/funny-pictures-service/src/test/java/com/optigra/funnypictures/service/marking/PictureAnnotatingServiceTest.java
@@ -0,0 +1,19 @@
+package com.optigra.funnypictures.service.marking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PictureAnnotatingServiceTest {
+	
+	@InjectMocks
+	private PictureAnnotatingService unit;
+	
+	@Test
+	public void testName() throws Exception {
+		
+	}
+
+}
