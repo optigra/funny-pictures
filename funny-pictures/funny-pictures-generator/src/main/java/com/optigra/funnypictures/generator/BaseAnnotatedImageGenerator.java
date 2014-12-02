819ee31329758002c41309fe86655da44b07a61e
Myroslav Golub <myroslav.golub@optigra-soft.com>
2014-12-02 19:10:18 +0200

    Created a stub for the image annotator.

---
+++ funny-pictures/funny-pictures-generator/src/main/java/com/optigra/funnypictures/generator/BaseAnnotatedImageGenerator.java
@@ -0,0 +1,20 @@
+package com.optigra.funnypictures.generator;
+
+import com.optigra.funnypictures.generator.api.AnnotatedImageGenerator;
+import com.optigra.funnypictures.generator.api.ImageAnnotationContext;
+import com.optigra.funnypictures.generator.api.ImageHandle;
+
+/**
+ * Generator of annotated images.
+ * @author odisseus
+ *
+ */
+public class BaseAnnotatedImageGenerator implements AnnotatedImageGenerator {
+
+	@Override
+	public ImageHandle generate(final ImageAnnotationContext context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ funny-pictures/funny-pictures-generator/src/test/java/com/optigra/funnypictures/generator/BaseAnnotatedImageGeneratorTest.java
@@ -0,0 +1,13 @@
+package com.optigra.funnypictures.generator;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BaseAnnotatedImageGeneratorTest {
+	
+	@InjectMocks
+	private BaseAnnotatedImageGenerator unit;
+
+}
