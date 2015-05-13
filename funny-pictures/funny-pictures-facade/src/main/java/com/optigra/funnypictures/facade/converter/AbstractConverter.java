53f34688530ebd901093059796c89597c7fe260e
Myroslav Golub <myroslav.golub@optigra-soft.com>
2015-05-13 21:58:56 +0300

    Added a temporary fix for incomplete JSON request issue.

--- funny-pictures/funny-pictures-facade/src/main/java/com/optigra/funnypictures/facade/converter/AbstractConverter.java
+++ funny-pictures/funny-pictures-facade/src/main/java/com/optigra/funnypictures/facade/converter/AbstractConverter.java
@@ -1,7 +1,11 @@
 package com.optigra.funnypictures.facade.converter;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Abstract convert class.
@@ -14,9 +18,17 @@ import java.util.List;
  *            Result of converting.
  */
 public abstract class AbstractConverter<SOURCE, TARGET> implements Converter<SOURCE, TARGET> {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(AbstractConverter.class);
 
 	@Override
 	public List<TARGET> convertAll(final List<SOURCE> sources) {
+
+		// TODO eliminate all cases when null is passed to this method
+		if (sources == null) {
+			LOG.warn("convertAll(null) called. This should never happen.");
+			return Collections.<TARGET>emptyList();
+		}
 
 		return convertAll(sources, new ArrayList<TARGET>(sources.size()));
 	}
